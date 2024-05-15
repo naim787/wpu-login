32819e4b48cf03fc6bf21c45a0ff0a2904bde4e1
Naim Abdullah <123891191+naim787@users.noreply.github.com>
2024-05-15 18:41:15 +0800

    membuat file  resolusi.txt

---
+++ ngetes-github-resolution.go
@@ -0,0 +1,14 @@
+package main
+import (
+  "fmt"
+)
+
+func main(){
+  fmt.Println("
+              hallo pak salah kenal, nama saya naim, saya lagi belajar pake github
+              Resolution saya :
+              - membuat bahasa pemrograman saya sendiri
+              - iggin bekerja di Mocrosoft/Google
+              
+              ")
+}
